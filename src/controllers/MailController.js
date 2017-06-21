095932e14aebbf3c7fd7dce5d9a250113c174faf
Nico Hauser <me@tyratox.ch>
2017-06-21 19:01:53 +0200

    Moar debugging

--- src/controllers/MailController.js
+++ src/controllers/MailController.js
@@ -33,6 +33,7 @@ class MailController {
 		bcc = [],
 		replyTo = null
 	) {
+		console.log("sendMail");
 		return new Promise((resolve, reject) => {
 			let data = {
 				from:
@@ -82,7 +83,10 @@ class MailController {
 				}
 			}
 
+			console.log("sending: " + data);
+
 			this.transporter.sendMail(data, (err, info) => {
+				console.log(err, info);
 				if (err) {
 					return reject(err);
 				}
