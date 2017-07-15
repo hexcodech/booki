b53022bd76fc9ae205c5491d6e41f38bdd699129
Nico Hauser <me@tyratox.ch>
2017-07-15 13:22:36 +0200

    Bugfixes in clean command

--- src/controllers/SystemController.js
+++ src/controllers/SystemController.js
@@ -8,6 +8,8 @@ class SystemController {
 		this.folders = folders;
 
 		this.os = require("os");
+		this.fs = require("fs");
+		this.async = require("async");
 		this.exec = require("child_process").exec;
 
 		bindAll(this, ["getStats", "cleanup"]);
@@ -87,27 +89,9 @@ class SystemController {
 					{ type: this.sequelize.QueryTypes.SELECT }
 				)
 				.then(images => {
-					let ids = images.map(image => {
-						return image.id;
-					});
+					let ids = images.map(image => image.id);
 
-					if (request.query.check) {
-						response.json({ deletable: ids });
-						return response.end();
-					} else {
-						//trigger hooks (hopefully)
-						return this.models.Images
-							.destroy({
-								where: {
-									id: {
-										$in: ids
-									}
-								}
-							})
-							.then(() => {
-								return Promise.resolve(ids);
-							});
-					}
+					return Promise.resolve(ids);
 				})
 		);
 
@@ -129,9 +113,9 @@ class SystemController {
 								images = new Set(
 									images.map(Image => Image.get("File").get("path"))
 								);
-								let toDelete = new Set(actualImages.filter((image => !images.has(image)));
-
-								console.log("Diff", toDelete);
+								let toDelete = new Set(
+									actualImages.filter(image => !images.has(image))
+								);
 								resolve(Array.from(toDelete));
 							});
 					}
@@ -141,8 +125,37 @@ class SystemController {
 
 		Promise.all(promises)
 			.then(params => {
-				response.json({ deletedIds: params[0], unlinkedFiles: params[1] });
-				return response.end();
+				if (request.query.check) {
+					response.json({ deletableIds: params[0], unlinkedFiles: params[1] });
+					return response.end();
+				} else {
+					//trigger hooks (hopefully)
+					return this.models.Images
+						.destroy({
+							where: {
+								id: {
+									$in: ids
+								}
+							}
+						})
+						.then(() => {
+							async.each(
+								params[1],
+								(path, callback) => fs.unlink(path, callback),
+								err => {
+									if (err) {
+										next(err);
+									}
+
+									response.json({
+										deletedIds: params[0],
+										deletedUnlinkedFiles: params[1]
+									});
+									return response.end();
+								}
+							);
+						});
+				}
 			})
 			.catch(next);
 	}
